38da3609911c26838a15f90a20e23721168a5a7a
Alex Sander <sander.asro@outlook.com>
2016-02-16 03:07:53 -0200

    Aula 02 - Aplicação do padrão Assertion Concern No Shared Kernel, pra ser utilizado na manipulação de validações das regra de negócio do dominio.

---
+++ ModernWebStore/ModernWebStore.SharedKernel/Events/DomainEvent.cs
@@ -0,0 +1,32 @@
+﻿using ModernWebStore.SharedKernel.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWebStore.SharedKernel.Events
+{
+public   class DomainEvent : IDomainEvent
+    {
+        public static IContainer Container { get; set; }
+        public DateTime DateOccurred { get; private set; }
+
+
+        public static void Raise<T>(T args) where T : IDomainEvent
+        {
+            try
+            {
+                if (Container != null)
+                {
+                    var obj = Container.GetService(typeof(IHandler<T>));
+                    ((IHandler<T>)obj).Handle(args);
+                }
+            }
+            catch
+            {
+                //throw;
+            }
+        }
+    }
+}
---
+++ ModernWebStore/ModernWebStore.SharedKernel/IHandler.cs
@@ -0,0 +1,16 @@
+﻿using ModernWebStore.SharedKernel.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWebStore.SharedKernel
+{
+public    interface IHandler<T>:IDisposable where T :IDomainEvent
+    {
+        void Handle(T args);
+        IEnumerable<T> Notify();
+        bool HasNotifications();
+    }
+}
